46c222a937bcac24a26cf19b3226bfddefabb08b
Apar Agarwal <aparagarwal01@gmail.com>
2025-12-27 13:52:54 +0530

    feat: add user profile page and avatar display with styling

--- urlShortener/models/user.model.js
+++ urlShortener/models/user.model.js
@@ -25,6 +25,9 @@ const userSchema = new mongoose.Schema(
             trim: true,
             index: true
         },
+        avatarUrl: {
+            type: String
+        },
         password: {
             type: String,
             required: true,
