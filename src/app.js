36f38e9259efcd444a2e2c856ac54b3861bac7f7
Apar Agarwal <aparagarwal01@gmail.com>
2026-02-18 23:22:58 +0530

    feat: add static file serving for uploads directory

--- src/app.js
+++ src/app.js
@@ -5,12 +5,14 @@ import { requestLogger } from './middlewares/logger.middleware.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 import { ApiError } from './utils/ApiError.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
+app.use('/uploads', express.static(path.join(process.cwd(), 'tmp/uploads')));
 
 // Use custom middleware for logging requests
 app.use(requestLogger);
